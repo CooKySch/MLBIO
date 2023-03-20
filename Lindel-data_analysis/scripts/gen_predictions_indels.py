6630345d5b01ab4f1735c1c57fe0f26747673bf1
Ioanna Nika <ioanna@Ioannas-small-MacBook-Pro.local>
2023-03-20 16:53:15 +0100

    prediction scripts and mses per task

--- Lindel-data_analysis/scripts/gen_predictions_indels.py
+++ Lindel-data_analysis/scripts/gen_predictions_indels.py
@@ -7,14 +7,15 @@ import glob
 import datetime as dt
 import pickle as pkl
 from tensorflow import keras 
+import numpy as np
 
 
 
 def main():
     parser = argparse.ArgumentParser(description="Generate predictions for some encoded feautures")
     parser.add_argument('-f', '--file_encoded_features', dest='file_name', default= "data/Lindel_test.txt", type=str, help="File path of the encoded features")
-    parser.add_argument("-m", '--model', dest='model', type=str, help="File path of the model")
-    parser.add_argument('-o, --output_file_predictions', dest='output_file', default="data/predictions",type=str, help="File path of the output file")
+    parser.add_argument('-m', '--model', dest='model', default= "data/L2_indel.h5", type=str, help="File path of the model")
+    parser.add_argument('-o, --output_file_predictions', dest='output_file', default="data/predictions_indels",type=str, help="File path of the output file")
     args = parser.parse_args()
 
     # Read the encoded features
@@ -24,8 +25,9 @@ def main():
     feature_size = len(features) + 384
     data = df.iloc[:, 1:].astype('float32')
 
+
     #X = features, y = labels
-    X = data.iloc[:, :feature_size]
+    X = data.iloc[:,  len(features):feature_size]
     y = data.iloc[:, feature_size:]
 
     # Load the model h5 file with keras
@@ -33,9 +35,11 @@ def main():
 
     # Generate predictions
     predictions = model.predict(X)
-    print("Here are the predictions:")
-    print(predictions)
+    print(type(predictions))
 
+    # save the predictions with numpy
+
+    np.save(args.output_file, predictions)
 
 
 
